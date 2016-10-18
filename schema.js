12346f9198206fe079130b568219eafc110179e5
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-10-18 16:26:29 -0400

    Initial commit

---
+++ schema.js
@@ -0,0 +1,11 @@
+var Schema = {
+  studyData: {
+    id:{type: 'increments', nullable: false, primary: true},
+    app_id: {type: 'integer', nullable: false},
+    action_id: {type: 'integer', nullable: true},
+    user_id: {type: 'integer',nullable: false},
+    time_taken: {type: 'float', nullable: false},
+    is_password: {type: 'boolean', nullable: false}
+  }
+};
+module.exports = Schema;
---
+++ touchServer.js
@@ -0,0 +1,44 @@
+var _ = require('lodash');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+// body-parser middleware for handling request variables
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host     : '127.0.0.1',
+        user     : 'root',
+        password : 'bazzinga',
+        database : 'TouchIdStudy',
+        charset  : 'utf8'
+  }
+});
+
+var Bookshelf = require('bookshelf')(knex);
+
+var StudyData = Bookshelf.Model.extend({
+  tableName: 'studyData'
+});
+
+var StudyDataCollection = Bookshelf.Collection.extend({
+  model: StudyData
+});
+
+router.route('/getDump')
+  .get(function (req, res){
+    StudyDataCollection.forge()
+    .fetch()
+    .then(function(collection){
+      res.json({error: false, data: collecion.toJSON()});
+    })
+  });
+
+app.use('/superStore/api', router);
+app.listen(3000, function() {
+  console.log("✔ Express server listening on port %d in %s mode", 3000, app.get('env'));
+});
