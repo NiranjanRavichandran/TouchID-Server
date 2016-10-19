533735d586864d436d65b0a0a045e8520e19b9d3
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-10-19 00:30:40 -0400

    Logging enabled

--- touchServer.js
+++ touchServer.js
@@ -43,8 +43,9 @@ router.route('/getDump')
 
   router.route('/save')
   .post(function(req, res){
-    console.log('@@@',req);
+    console.log('@@@',req.body);
     var token = req.body.token;
+    console.log('###',req.body.token);
     if(token == "bazzinga") {
       StudyData.forge({
       user_id:req.body.userId,
