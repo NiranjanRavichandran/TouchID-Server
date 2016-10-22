1e29e7a2ea3a0874446be744f955c30866883212
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-10-22 18:55:48 -0400

    User Schema Bug fix

--- schema.js
+++ schema.js
@@ -9,16 +9,16 @@ var Schema = {
     is_password: {type: 'boolean', nullable: false}
   },
   users: {
-    user_id:{type: 'String', nullable: false, unique: true},
-    password:{type: 'String', nullable: false},
-    fname:{type: 'String', nullable: false},
-    lname:{type: 'String', nullable: false},
-    other_user1:{type: 'String', nullable: true},
-    other_user2:{type: 'String', nullable: true},
-    other_user3:{type: 'String', nullable: true},
-    other_user4:{type: 'String', nullable: true},
-    other_user5:{type: 'String', nullable: true},
-    other_user6:{type: 'String', nullable: true}
+    user_id:{type: 'string', nullable: false, unique: true},
+    password:{type: 'string', nullable: false},
+    fname:{type: 'string', nullable: false},
+    lname:{type: 'string', nullable: false},
+    other_user1:{type: 'string', nullable: true},
+    other_user2:{type: 'string', nullable: true},
+    other_user3:{type: 'string', nullable: true},
+    other_user4:{type: 'string', nullable: true},
+    other_user5:{type: 'string', nullable: true},
+    other_user6:{type: 'string', nullable: true}
   }
 };
 module.exports = Schema;
