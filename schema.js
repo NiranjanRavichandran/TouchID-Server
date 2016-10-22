8e626fb65242325353edf0f081e6ce572b993426
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-10-22 18:52:10 -0400

    Added user schema and dump API for users

--- schema.js
+++ schema.js
@@ -3,10 +3,22 @@ var Schema = {
     id:{type: 'increments', nullable: false, primary: true},
     app_id: {type: 'integer', nullable: false},
     action_id: {type: 'integer', nullable: true},
-    user_id: {type: 'integer',nullable: false},
+    user_id: {type: 'String',nullable: false},
     time_taken: {type: 'float', nullable: false},
     study_id: {type: 'integer', nullable: false},
     is_password: {type: 'boolean', nullable: false}
+  },
+  users: {
+    user_id:{type: 'String', nullable: false, unique: true},
+    password:{type: 'String', nullable: false},
+    fname:{type: 'String', nullable: false},
+    lname:{type: 'String', nullable: false},
+    other_user1:{type: 'String', nullable: true},
+    other_user2:{type: 'String', nullable: true},
+    other_user3:{type: 'String', nullable: true},
+    other_user4:{type: 'String', nullable: true},
+    other_user5:{type: 'String', nullable: true},
+    other_user6:{type: 'String', nullable: true}
   }
 };
 module.exports = Schema;
--- touchServer.js
+++ touchServer.js
@@ -25,8 +25,16 @@ var StudyData = Bookshelf.Model.extend({
   tableName: 'studyData'
 });
 
+var User = Bookshelf.Model.extend({
+  tableName: 'users'
+});
+
 var StudyDataCollection = Bookshelf.Collection.extend({
   model: StudyData
+});
+
+var Users = Bookshelf.Collection.extend({
+  model: User
 });
 
 router.route('/getDump')
@@ -64,6 +72,46 @@ router.route('/getDump')
       res.json({error: true, data:{message: 'Invalid Token'}});
   }
 }); //End of post
+
+router.route('/Users')
+.get(function(req, res){
+  Users.forge()
+  .fetch()
+  .then(function(collection){
+    res.json({error: false, data: collection.toJSON()});
+  })
+  .catch(function (err) {
+    res.status(500).json({error: true, data: {message: err.message}});
+  });
+});//End of get
+
+.post(function(req, res){
+  var token = req.body.token;
+  if(token == "bazzinga") {
+    User.forge({
+    user_id:req.body.userId,
+    password: req.body.password,
+    fname: req.body.fname,
+    lname: req.body.lname,
+    other_user1: req.body.user1,
+    other_user2: req.body.user2,
+    other_user3: req.body.user3,
+    other_user4: req.body.user4,
+    other_user5: req.body.user5,
+    other_user6: req.body.user6
+  })
+  .save()
+  .then(function(data){
+      res.json({error: false, data: {message: "Success"}});
+  })
+  .catch(function (err) {
+    res.status(500).json({error: true, data: {message: err.message}});
+  });
+}else {
+    res.json({error: true, data:{message: 'Invalid Token'}});
+}
+});
+
 app.use('/api', router);
 app.get('/', function(req, res){
   res.sendFile('TouchID-Server/main.html');
